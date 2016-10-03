cf82282a7ecd5e20838a177e41745e81a31d6b87
acomesanavila <acomesanavila@victoria7.danielcastelao.org>
2016-10-03 10:05:58 +0200

    area triangulo

---
+++ src/boletin2_1/Boletin2_1.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin2_1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author bvelosofioretti
+ */
+public class Boletin2_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+       float base; 
+       
+       float altura;
+      
+       float area;      
+       
+       base = Float.parseFloat(JOptionPane.showInputDialog("base"));
+       
+       altura = Float.parseFloat(JOptionPane.showInputDialog("altura"));
+       
+        area=base*altura/2;
+      
+        JOptionPane.showMessageDialog(null,"area =" + base);
+        
+        
+       
+       
+   
+       
+        
+        
+        
+    }
+    
+}
